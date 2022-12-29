082e28b0376367fb7082fd81c0f5aa83ab0f2792
Christopher Wellons <wellons@nullprogram.com>
2022-12-29 17:48:32 -0500

    Replace "clang++" with "c++" for asset generation
    This chooses the platform's standard C++ compiler whether that is
    clang++ or g++.

--- tools/create_assets.py
+++ tools/create_assets.py
@@ -93,7 +93,7 @@ def encode_bin(data):
 
 def encode_font(fontPath):
 	if not os.path.isfile("./tools/font2inl.out"):
-		result = subprocess.run(['clang++', 'tools/font2inl.cpp', '-o', 'tools/font2inl.out'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+		result = subprocess.run(['c++', 'tools/font2inl.cpp', '-o', 'tools/font2inl.out'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 		print(result.stdout.decode('utf-8'))
 		if not os.path.isfile("./tools/font2inl.out"):
 			print("Cannot compile tools/font2inl.cpp for compressing font!")
